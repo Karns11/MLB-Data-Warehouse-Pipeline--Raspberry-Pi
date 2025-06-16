048b8fa641820d8e512ae9b8c6fc55ee9c6b98d9
karns11 <nathankarns28@gmail.com>
2025-06-16 16:15:18 -0400

    add is_currently_qualified task to dag

--- dags/run_r_script_dag.py
+++ dags/run_r_script_dag.py
@@ -414,6 +414,64 @@ def append_team_game_stats(date_str: str):
         conn.execute(query)
 
 
+def update_is_currently_qualified_field(date_str: str):
+    # Create SQLAlchemy engine
+    engine = create_engine('postgresql+psycopg2://root:root@my_postgres:5432/baseballr_db')
+
+    query = f"""
+        ALTER TABLE warehouse.fact_player_stats
+        ADD COLUMN is_currently_qualified SMALLINT DEFAULT 0;
+
+        WITH qualified_players AS (
+        SELECT 
+            fps.player_sk,
+            fps.team_sk,
+            fps.as_of_date
+        FROM warehouse.fact_player_stats fps
+        JOIN warehouse.fact_team_games_history ftgh
+            ON fps.team_sk = ftgh.team_sk
+        AND fps.as_of_date = ftgh.as_of_date
+        WHERE 1=1
+            AND fps.as_of_date = (SELECT MAX(as_of_Date) FROM warehouse.fact_player_stats)
+            AND fps.pa >= 3.1 * ftgh.gp
+        )
+        UPDATE warehouse.fact_player_stats fps
+        SET is_currently_qualified = 1
+        FROM qualified_players qp
+        WHERE fps.player_sk = qp.player_sk
+        AND fps.team_sk = qp.team_sk
+        AND fps.as_of_date = qp.as_of_date;
+
+        
+
+        UPDATE warehouse.fact_player_stats_history
+        SET is_currently_qualified = 0;
+
+        WITH qualified_players AS (
+        SELECT 
+            fpsh.player_sk,
+            fpsh.team_sk,
+            fpsh.as_of_date
+        FROM warehouse.fact_player_stats_history fpsh
+        JOIN warehouse.fact_team_games_history ftgh
+            ON fpsh.team_sk = ftgh.team_sk
+        AND fpsh.as_of_date = ftgh.as_of_date
+        WHERE 1=1
+            AND fpsh.as_of_date = (SELECT MAX(as_of_Date) FROM warehouse.fact_player_stats_history)
+            AND fpsh.pa >= 3.1 * ftgh.gp
+        )
+        UPDATE warehouse.fact_player_stats_history fpsh
+        SET is_currently_qualified = 1
+        FROM qualified_players qp
+        WHERE fpsh.player_sk = qp.player_sk
+        AND fpsh.team_sk = qp.team_sk
+        AND fpsh.as_of_date = qp.as_of_date;
+    """
+
+    with engine.begin() as conn:
+        conn.execute(query)
+
+
 AIRFLOW_HOME = os.environ.get("AIRFLOW_HOME", "/opt/airflow")
 
 default_args = {
@@ -424,8 +482,8 @@ dag = DAG(
     'run_r_script_daily',
     default_args=default_args,
     description='Run an R script every day at 10AM',
-    schedule_interval='0 14 * * *', 
-    start_date=datetime(2025, 6, 13),
+    schedule_interval='0 14 * * *',  #None
+    start_date=datetime(2025, 6, 14),
     catchup=False,
     tags=['rscript'],
 )
@@ -434,8 +492,8 @@ dag = DAG(
 # --- CONFIG ---
 BACKFILL_FLAG = 0
 
-START_DATE = datetime.strptime("2025-05-28", "%Y-%m-%d") # 03-20 then 03-28 then #07-19
-END_DATE = datetime.strptime("2025-06-12", "%Y-%m-%d") # 03-21 then 07-14 then #09-30
+START_DATE = datetime.strptime("2025-05-02", "%Y-%m-%d") # 03-20 then 03-28 then #07-19
+END_DATE = datetime.strptime("2025-06-13", "%Y-%m-%d") # 03-21 then 07-14 then #09-30
 
 date_range = [
     (START_DATE + timedelta(days=i)).strftime("%Y-%m-%d")
@@ -535,11 +593,18 @@ for date in date_range:
         op_args=[date],
         dag=dag,
     )
+
+    run_update_is_currently_qualified_field = PythonOperator(
+        task_id=f"update_is_currently_qualified_field_{date}",
+        python_callable=update_is_currently_qualified_field,
+        op_args=[date],
+        dag=dag,
+    )
     
     
 
     # Run dbt after R script for the same date
-    run_r_script >> run_add_people_to_staging >> run_update_people_dim >> run_insert_people_dim_data >> run_add_team_to_staging >> run_update_team_dim >> run_insert_team_dim_data >> run_dbt >> run_add_on_surrogate_keys_to_fact >> run_append_stats >> run_add_surrogate_keys_to_team_games_fact >> run_append_team_game_stats
+    run_r_script >> run_add_people_to_staging >> run_update_people_dim >> run_insert_people_dim_data >> run_add_team_to_staging >> run_update_team_dim >> run_insert_team_dim_data >> run_dbt >> run_add_on_surrogate_keys_to_fact >> run_append_stats >> run_add_surrogate_keys_to_team_games_fact >> run_append_team_game_stats >> run_update_is_currently_qualified_field
 
 
     if previous_task:
